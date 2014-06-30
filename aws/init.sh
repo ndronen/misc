37c1761a8c4de061a59abdec1352bd822fc5a28b
Nicholas Dronen <ubuntu@ip-10-146-211-103.ec2.internal>
2014-06-30 10:53:18 -0600

    Added git config commands.

--- aws/init.sh
+++ aws/init.sh
@@ -5,4 +5,7 @@ sudo apt-file update
 sudo apt-get install gcc g++ libgfortran3 liblapack3 parallel 
 sudo apt-get install graphicsmagick python-pgmagick
 
+git config --global user.name "Nicholas Dronen"
+git config --global user.emai ndronen@gmail.com
+
 git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
