486915dbc780a36f669fc50074620461565a7f34
Nicholas Dronen <ndronen@gmail.com>
2014-06-30 14:27:11 -0600

    Fixed git --config typo.

--- aws/init.sh
+++ aws/init.sh
@@ -6,6 +6,6 @@ sudo apt-get install gcc g++ libgfortran3 liblapack3 parallel
 sudo apt-get install graphicsmagick python-pgmagick
 
 git config --global user.name "Nicholas Dronen"
-git config --global user.emai ndronen@gmail.com
+git config --global user.email ndronen@gmail.com
 
 git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
