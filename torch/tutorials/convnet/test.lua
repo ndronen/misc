b3bf969b53928d563c98386f629226e88ce760ad
Nicholas Dronen <nicholas.dronen@pearson.com>
2015-06-03 13:58:18 -0600

    Got test.lua to record indices and values of temporal pooling layer.

--- torch/tutorials/convnet/test.lua
+++ torch/tutorials/convnet/test.lua
@@ -10,22 +10,32 @@ cmd:text('Options:')
 cmd:argument('-model', 'path to the model')
 cmd:argument('-input', 'path to test data file (HDF5 format)')
 cmd:argument('-output', 'path to which to save the predictions')
+cmd:option('-record', false,
+  'whether to record indices/values of activations of first convolutional layer')
 cmd:text()
 
 local opt = cmd:parse(arg or {})
-
-print('opt.model ' .. opt.model)
-print('opt.input ' .. opt.input)
-print('opt.output ' .. opt.output)
 
 -- Load data and model.
 local testFile = hdf5.open(opt.input, 'r')
 local testData = testFile:read():all()
 local model = torch.load(opt.model)
 
--- Get and save predictions.
-local pred = kttorch.predict(model, testData.X)
 local predOut = hdf5.open(opt.output, 'w')
+
+-- Get and save predictions.
+local pred = nil
+if opt.indices then
+  modules = model:findModules('nn.TemporalMaxPooling')
+  assert(#modules == 1)
+  indexRecorder = kttorch.IndexRecorder(modules[1])
+  outputRecorder = kttorch.OutputRecorder(modules[1])
+  pred = kttorch.predict(model, testData.X, { indexRecorder, outputRecorder })
+  predOut:write('indices', indexRecorder.recording)
+  predOut:write('output', outputRecorder.recording)
+else
+  pred = kttorch.predict(model, testData.X)
+end
+
 predOut:write('pred', pred)
 predOut:close()
-
