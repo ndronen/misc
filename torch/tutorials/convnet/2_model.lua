0735347613e092df59cbafe7b8c477518bcf6e67
Nicholas Dronen <nicholas.dronen@pearson.com>
2015-06-16 09:23:35 -0600

    Added spatial convolutional network to 2_model.lua, added kttorch.InputPrinter instances.

--- torch/tutorials/convnet/2_model.lua
+++ torch/tutorials/convnet/2_model.lua
@@ -36,7 +36,11 @@ if string.match(opt.nFullyConnectedLayers, ",") then
     table.insert(opt.nFullyConnectedLayers, tonumber(size))
   end
 else
-  opt.nFullyConnectedLayers = { tonumber(opt.nFullyConnectedLayers) }
+  if opt.nFullyConnectedLayers ~= "0" then
+    opt.nFullyConnectedLayers = { tonumber(opt.nFullyConnectedLayers) }
+  else 
+    opt.nFullyConnectedLayers = {}
+  end
 end
 
 local activation = nil
@@ -91,15 +95,75 @@ end
 lookupTable.weight:renorm(2, 1, opt.maxWordNorm)
 model:add(lookupTable)
 
-if opt.spatial then
-  -- This doesn't work yet.
-  model:add(nn.SpatialConvolution(inputFrameSize, opt.nKernels, kw, kw))
-  model:add(activation())
-  model:add(nn.SpatialMaxPooling(kw, kw))
+local penultimateOutput = k * opt.nKernels
+local ninput = penultimateOutput
 
-  model:add(nn.SpatialConvolution(opt.nKernels, 5, kw, kw))
+if opt.spatial then
+  print('trainData.data:size(2)')
+  print(trainData.data:size(2))
+  model:add(nn.View(-1, trainData.data:size(2), opt.wordDims))
+
+  -- A spatial convolution can take either grayscale (i.e. single-channel)
+  -- or multi-channel (e.g. RGB, YUV) images.  When the input is a sentence
+  -- matrix, there is only a single channel; hence, the first argument to
+  -- the first layer's nn.SpatialConvolution(MM) is fixed to 1.
+  local l1NFilters = 3
+  local l1kW = 15
+  local l1kH = 5
+  -- model:add(kttorch.InputPrinter('SpatialConvolutionMM #1'))
+  model:add(nn.SpatialConvolutionMM(1, l1NFilters, l1kW, l1kH))
   model:add(activation())
-  model:add(nn.SpatialMaxPooling(kw, kw))
+  -- expected output: 5 X 41 X 11
+  -- actual output:   5 X 41 X 11
+
+  local l1PoolW = 2
+  local l1PoolH = 5
+  -- model:add(kttorch.InputPrinter('SpatialMaxPooling #1'))
+  model:add(nn.SpatialMaxPooling(l1PoolW, l1PoolH, 1, 1))
+  -- expected output: 5 X 37 X 10
+  -- actual output:   5 X 37 X 10
+
+  local l2NFilters = 15
+  local l2kW = 5
+  local l2kH = 10
+  -- model:add(kttorch.InputPrinter('SpatialConvolutionMM #2'))
+  model:add(nn.SpatialConvolutionMM(l1NFilters, l2NFilters, l2kW, l2kH))
+  model:add(activation())
+  -- These sizes don't seem right.  Shouldn't the number of feature maps
+  -- output by this convolution be #feature maps from previous layer X
+  -- number of filters in this layer?
+  -- expected output: 15 X 28 X 6
+  -- actual output:  15 X 28 X 6
+
+  local l2PoolW = 3
+  local l2PoolH = 9
+  -- model:add(kttorch.InputPrinter('SpatialMaxPooling #2'))
+  model:add(nn.SpatialMaxPooling(l2PoolW, l2PoolH, 1, 1))
+  -- output 15 X 20 X 4
+
+  -- I this this might have become 15 parallel temporal convolutions
+  -- taking 2D inputs of size 20 X 5.
+  p = nn.Parallel(1, 1)
+  for i=1,l2NFilters do
+    local tNInputs = 4
+    local tNFilters = 1
+    local tKW = 3
+    s = nn.Sequential()
+    -- s:add(kttorch.InputPrinter('TemporalConvolution #' .. i))
+    s:add(nn.TemporalConvolution(tNInputs, tNFilters, tKW))
+    -- output 20 X 4
+    -- s:add(kttorch.InputPrinter('TemporalMaxPooling #' .. i))
+    s:add(nn.TemporalMaxPooling(18))
+
+    p:add(s)
+  end
+  model:add(p)
+
+  -- model:add(nn.Dropout(0.5))
+  -- model:add(kttorch.InputPrinter('View (output of Parallel)'))
+  ninput = 15
+  penultimateOutput = 15
+  model:add(nn.View(penultimateOutput))
 else
   -- if opt.type == 'cuda' then
   --  model:add(nn.TemporalConvolutionFB(inputFrameSize, opt.nKernels, kw, dw))
@@ -118,19 +182,15 @@ else
   else
     model:add(nn.TemporalKMaxPooling(k))
   end
+  -- model:add(nn.BatchNormalization(0))
+  model:add(nn.Dropout(0.5))
+  model:add(nn.View(k * opt.nKernels))
 end
 
--- model:add(nn.BatchNormalization(0))
-model:add(nn.Dropout(0.5))
-model:add(nn.View(k * opt.nKernels))
-
 -- stage 3: fully-connected layers
-local penultimateOutput = nil
+
 for i,noutput in ipairs(opt.nFullyConnectedLayers) do
-  local ninput = nil
-  if i == 1 then
-    ninput = k * opt.nKernels
-  else
+  if i > 1 then
     ninput = opt.nFullyConnectedLayers[i-1]
   end
   -- model:add(nn.BatchNormalization(0))
@@ -151,6 +211,7 @@ else
 end
 
 -- model:add(nn.BatchNormalization(0))
+-- model:add(kttorch.InputPrinter('Final Linear'))
 model:add(nn.Linear(penultimateOutput, noutputs))
 
 if opt.type == 'cuda' then
