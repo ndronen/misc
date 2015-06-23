5596c4a4dab6636c70e619e0f57c6cb217b92ac9
Nicholas Dronen <nicholas.dronen@pearson.com>
2015-06-23 09:54:22 -0600

    Adding scripts for training using Collobert & Weston style negative examples (sentences with one word randomly replaced) with increasing training set sizes.

---
+++ torch/tutorials/convnet/results/test-wiki/collobert/1m/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -xe
+
+RESULTS="$(dirname -- $(readlink -f -- "$0"))"
+
+# OK, *almost* one million.
+
+th bin/run.lua 150001 data/sents-okanohara-wiki-dlm-not-used-train-1000000-idx.h5 \
+    -save $RESULTS \
+    -type cuda \
+    -nKernels 250 \
+    -kernelWidth 4 \
+    -nFullyConnectedLayers 500,200 \
+    -learningRate 0.1 \
+    -momentum 0.9 \
+    -batchSize 128 \
+    -test \
+    -testFile data/sents-okanohara-wiki-dlm-not-used-test-0020000-idx.h5 \
+    -nTrain 999999 \ 
+    -lookupTableDropout 0.0 \
+    -makeNegativeExamples
