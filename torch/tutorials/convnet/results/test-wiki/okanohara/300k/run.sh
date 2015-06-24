d32242aeca45f9dfdda2ec85b683cb7cc683deac
Nicholas Dronen <nicholas.dronen@pearson.com>
2015-06-24 15:33:26 -0600

    Added scripts for running Wiki+Okanohara.

---
+++ torch/tutorials/convnet/results/test-wiki/okanohara/300k/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -xe
+
+RESULTS="$(dirname -- $(readlink -f -- "$0"))"
+
+th bin/run.lua 150001 /tmp/sents-okanohara-wiki-dlm-train-initial-1867900-idx.h5 \
+    -save $RESULTS \
+    -type cuda \
+    -nKernels 250 \
+    -kernelWidth 4 \
+    -nFullyConnectedLayers 500,200 \
+    -learningRate 0.1 \
+    -momentum 0.9 \
+    -batchSize 128 \
+    -test \
+    -testFile /tmp/sents-okanohara-wiki-dlm-test-initial-40000-idx.h5 \
+    -nTrain 300000 \
+    -lookupTableDropout 0.0
