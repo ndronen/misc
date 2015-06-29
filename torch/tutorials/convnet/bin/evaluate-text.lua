520cc3d3b31cccd1959f2f61f20f8bd41819f526
Nicholas Dronen <nicholas.dronen@pearson.com>
2015-06-29 16:38:43 -0600

    Got evaluate-text.lua working with file inputs and various formatting options.

--- torch/tutorials/convnet/bin/evaluate-text.lua
+++ torch/tutorials/convnet/bin/evaluate-text.lua
@@ -2,12 +2,15 @@
 
 require 'convnet.utils';
 
-local function makeLine(model, modelInfo, sentence)
+local function makeLine(model, modelInfo, sentence, opt)
   local output = torch.exp(model:forward(convertSentenceToIndices(sentence, modelInfo)))
   local m, i = torch.max(output, 1)
   local line = string.format('%d', i[1])
   for j=1,output:size(1) do
-    line = line .. string.format(',%0.6f', output[j])
+    line = line .. string.format(opt.sep .. '%0.6f', output[j])
+  end
+  if opt.printSentence then
+    line = line .. opt.sep .. sentence
   end
   return line
 end
@@ -20,7 +23,9 @@ cmd:text('Options:')
 cmd:argument('-modelPath', 'path to the model')
 cmd:argument('-modelInfoPath', 'path to the .json file containing metadata (vocabulary, etc.)')
 cmd:argument('-sentence', 'a sentence or a path to a file containing one sentence per line')
-cmd:option('-header', false, 'include header in output')
+cmd:option('-printHeader', false, 'include header in output')
+cmd:option('-printSentence', false, 'include sentence in output')
+cmd:option('-sep', '\t', 'separator for output')
 cmd:text()
 
 local opt = cmd:parse(arg or {})
@@ -32,17 +37,20 @@ local lines = {}
 if fileExists(opt.sentence) then
   f = io.open(opt.sentence, 'r')
   for sentence in io.lines(opt.sentence) do
-    table.insert(lines, makeLine(model, modelInfo, sentence))
+    table.insert(lines, makeLine(model, modelInfo, sentence, opt))
   end
 else
-  table.insert(lines, makeLine(model, modelInfo, sentence))
+  table.insert(lines, makeLine(model, modelInfo, opt.sentence, opt))
 end
 
-if opt.header then
+if opt.printHeader then
   local noutput = model.modules[#model.modules-1].weight:size(1)
   local header = 'predicted'
   for j=1,noutput do
-    header = header .. ',output' .. tostring(j)
+    header = header .. opt.sep .. 'output' .. tostring(j)
+  end
+  if opt.printSentence then
+    header = header .. opt.sep .. 'sentence'
   end
   print(header)
 end
