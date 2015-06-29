916149cb88fbf21b6a6f1175519dceb44b806860
Nicholas Dronen <nicholas.dronen@pearson.com>
2015-06-29 14:57:53 -0600

    Make printing the header optional.

--- torch/tutorials/convnet/bin/evaluate-sentence.lua
+++ torch/tutorials/convnet/bin/evaluate-sentence.lua
@@ -2,7 +2,6 @@
 
 require 'convnet.utils';
 
-
 local cmd = torch.CmdLine()
 cmd:text()
 cmd:text('Predict using a saved model')
@@ -10,26 +9,28 @@ cmd:text()
 cmd:text('Options:')
 cmd:argument('-modelPath', 'path to the model')
 cmd:argument('-modelInfoPath', 'path to the .json file containing metadata (vocabulary, etc.)')
-cmd:argument('-s', 'the sentence to evaluate')
+cmd:argument('-sentence', 'the sentence to evaluate')
+cmd:option('-header', false, 'include header in output')
 cmd:text()
 
 local opt = cmd:parse(arg or {})
 
 local model, modelInfo = loadModelAndModelInfo(opt.modelPath, opt.modelInfoPath)
 
-local output = torch.exp(model:forward(convertSentenceToIndices(opt.s, modelInfo)))
+local output = torch.exp(model:forward(convertSentenceToIndices(opt.sentence, modelInfo)))
 
-local header = 'max,predicted class,'
-for j=1,output:nElement() do
-  header = header .. ',softmax' .. tostring(i)
+if opt.header then
+  local header = 'predicted'
+  for j=1,output:nElement() do
+    header = header .. ',softmax' .. tostring(j)
+  end
+  print(header)
 end
-
 local m, i = torch.max(output, 1)
 
-print(header)
-
-local line = string.format('%0.6f,%d', m[1], i[1])
+local line = string.format('%d', i[1])
 for j=1,output:nElement() do
   line = line .. string.format(',%0.6f', output[j])
 end
+
 print(line)
