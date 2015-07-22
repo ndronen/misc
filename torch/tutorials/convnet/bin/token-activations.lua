e1d23ac610e188b874a2aa43c0324e645d843225
Nicholas Dronen <nicholas.dronen@pearson.com>
2015-07-22 14:58:15 -0600

    Added options to selectively output prediction, softmax output, and activations.

--- torch/tutorials/convnet/bin/token-activations.lua
+++ torch/tutorials/convnet/bin/token-activations.lua
@@ -1,5 +1,6 @@
 #!/usr/bin/env th
 
+
 require 'hdf5';
 require 'convnet.utils';
 require 'convnet.inspect';
@@ -19,13 +20,27 @@ local buildTokenActivations = function(tokens, posPolarity, negPolarity, posMagn
   return t
 end
 
-local showTokenActivations = function(tokens, posPolarity, negPolarity, posMagnitude, negMagnitude)
+local printTokenActivations = function(tokens, posPolarity, negPolarity, posMagnitude, negMagnitude, printLegend, printHeader)
   local tokActs = buildTokenActivations(
       tokens,
       posPolarity, negPolarity,
       posMagnitude, negMagnitude)
+
+  if printHeader then
+    if printLegend then
+      print("Activations ['N' = number, 'S' = sum, 'acts' = activations, '+' = positive, '-' = negative]:")
+    else
+      print("Activations:")
+    end
+
+    local header = string.format("%20s     %10s %10s %8s %8s",
+      "Token", "N+ acts.", "N- acts.",
+      "S+ acts.", "S- acts.")
+    print(header)
+  end
+
   for i=1,#tokActs do
-    local output = string.format("%20s     %2d %2d  %0.2f  %0.2f", 
+    local output = string.format("%20s     %10d %10d %8.2f %8.2f", 
       tokActs[i].token,
       tokActs[i].posPolarity, tokActs[i].negPolarity,
       tokActs[i].posMagnitude, tokActs[i].negMagnitude)
@@ -46,18 +61,47 @@ tokenActivations = function(model, modelInfo, filterInfo, sentence, opt)
   local posPolarity, negPolarity = countPolarityOfActivations(recording, input, filterInfo.positiveFilters, filterInfo.negativeFilters)
   local posMagnitude, negMagnitude = sumMagnitudeOfActivations(recording, input, filterInfo.positiveFilters, filterInfo.negativeFilters)
 
-  print('pred')
-  print(pred)
-  if output:nElement() > 1 then
-    print('output')
-    print(torch.exp(output))
+  local numPrints = opt.pred and 1 or 0
+  numPrints = numPrints + (opt.output and 1 or 0) + (opt.activations and 1 or 0)
+  local printHeader = numPrints > 1
+
+  if opt.pred then
+    if printHeader then
+      print("Pred: " .. pred[1])
+    else
+      print(pred[1])
+    end
   end
-  showTokenActivations(tokens, posPolarity, negPolarity, posMagnitude, negMagnitude)
+
+  if opt.output then
+    if output:nElement() > 1 then
+      local output = torch.exp(output)
+      local line = ''
+      for i=1,output:size(1) do
+        line = line .. output[i]
+        if i < output:size(1) then
+          line = line .. ','
+        end
+      end
+      if printHeader then
+        print("Output: " .. line)
+      else
+        print(line)
+      end
+    end
+  end
+
+  if opt.activations then
+    printTokenActivations(tokens,
+      posPolarity, negPolarity,
+      posMagnitude, negMagnitude,
+      opt.verbose, printHeader)
+  end
 end
 
 local cmd = torch.CmdLine()
 cmd:text()
-cmd:text('Classify filters of a convnet')
+cmd:text('Get prediction, softmax output, and per-token activation information for sentences')
 cmd:text()
 cmd:text('Options:')
 cmd:argument('-model', 'path to a serialized model')
@@ -66,12 +110,15 @@ cmd:argument('-filterInfo', 'path to file to which to write output')
 cmd:argument('-sentence', 'sentence to be processed')
 cmd:option('-gcFreq', 100, 'frequency of garbage collection calling kttorch.predict')
 cmd:option('-verbose', false, 'print more information to terminal during execution')
+cmd:option('-pred', false, "print the model's prediction")
+cmd:option('-output', false, "print the model's (softmax) output")
+cmd:option('-activations', false, "print per-token activations")
 cmd:text()
 
 local opt = cmd:parse(arg or {})
 
 -- Set model, model info, input, etc.
-local model = torch.load(opt.model)
+local model = loadModel(opt.model)
 local modelInfo = loadModelInfo(opt.modelInfo)
 local filterInfoFile = hdf5.open(opt.filterInfo)
 local filterInfo = filterInfoFile:read():all()
@@ -84,4 +131,3 @@ if fileExists(opt.sentence) then
 else
   tokenActivations(model, modelInfo, filterInfo, opt.sentence, opt)
 end
-
