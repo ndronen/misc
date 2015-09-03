c8b678934a43b39a687fe4327e9e100913a9b2bb
Nicholas Dronen <ndronen@gmail.com>
2015-09-03 14:08:00 -0600

    Change train.py to allow loading of labels.

---
+++ keras/train.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python
+
+import os, sys, shutil
+import argparse
+import logging
+import json
+import uuid
+
+import numpy as np
+import h5py
+import six
+from sklearn.metrics import classification_report, confusion_matrix, f1_score
+
+from keras.utils import np_utils
+from keras.optimizers import SGD
+import keras.callbacks
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+#from nick.callbacks import SklearnMetricCheckpoint
+
+sys.path.append('.')
+
+class ClassificationReport(keras.callbacks.Callback):
+    def __init__(self, x, y, logger, msg='', labels=None):
+        self.x = x
+        self.y = y
+        self.logger = logger
+        self.msg = msg
+        self.labels = labels
+
+    def on_epoch_end(self, batch, logs={}):
+        y_hat = self.model.predict_classes(self.x, verbose=0)
+        report = classification_report(
+                self.y, y_hat, labels=self.labels)
+        if self.msg and len(self.msg):
+            self.logger(self.msg)
+        self.logger(report)
+
+class ConfusionMatrix(keras.callbacks.Callback):
+    def __init__(self, x, y, logger, msg='', labels=None):
+        self.x = x
+        self.y = y
+        self.logger = logger
+        self.msg = msg
+        self.labels = labels
+
+    def on_epoch_end(self, batch, logs={}):
+        y_hat = self.model.predict_classes(self.x, verbose=0)
+        report = confusion_matrix(
+                self.y, y_hat, labels=self.labels)
+        if self.msg and len(self.msg):
+            self.logger(self.msg)
+        self.logger(report)
+
+class ModelConfig:
+    def __init__(self, **entries): 
+        self.__dict__.update(entries)
+
+    def __repr__(self):
+        return '<%s>' % \
+            str('\n '.join('%s : %s' % \
+                (k, repr(v)) for (k, v) in self.__dict__.iteritems()))
+
+class LoggerWriter:
+    def __init__(self, level):
+        # self.level is really like using log.debug(message)
+        # at least in my case
+        self.level = level
+
+    def write(self, message):
+        # if statement reduces the amount of newlines that are
+        # printed to the logger
+        if message != '\n':
+            self.level(message)
+                
+    def flush(self):
+        # create a flush method so things can be flushed when
+        # the system wants to. Not sure if simply 'printing'
+        # sys.stderr is the correct way to do it, but it seemed
+        # to work properly for me.
+        self.level(sys.stderr)
+
+def fake_print(s):
+    print(s)
+
+"""
+Train a model on sentences.
+"""
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+            description='Train a convnet on sentences.')
+    parser.add_argument('model_dir', metavar="MODEL_DIR", type=str,
+            help='The base directory of this model.  Must contain a model.py (model code) and a model.json (hyperparameters).  Model configuration and weights are saved to model_dir/UUID.')
+    parser.add_argument('train_file', metavar='TRAIN_FILE', type=str,
+            help='HDF5 file of training examples.')
+    parser.add_argument('validation_file', metavar='VALIDATION_FILE', type=str,
+            help='HDF5 file of validation examples.')
+    parser.add_argument('target_name', metavar='TARGET_NAME', type=str,
+            help='Name of the target variable in input HDF5 file.')
+
+    parser.add_argument('--model-dest', type=str,
+            help='Directory to which to copy model.py and model.json.  This overrides copying to model_dir/UUID.')
+    parser.add_argument('--labels', type=str,
+            help='Pickled dictionary of labels from sklearn.preprocessing.LabelEncoder.  For labels to be used, the dictionary must contain a key `TARGET_NAME`')
+    parser.add_argument('--description', type=str,
+            help='Short description of this model (data, hyperparameters, etc.)')
+    parser.add_argument('--seed', default=1, type=int,
+            help='The seed for the random number generator')
+    parser.add_argument('--n-train', default=np.inf, type=int,
+            help='The number of training examples to use')
+    parser.add_argument('--n-validation', default=np.inf, type=int,
+            help='The number of validation examples to use')
+    parser.add_argument('--n-vocab', default=-1, type=int,
+            help='The number of words in the training vocabulary')
+    parser.add_argument('--log', action='store_true',
+            help='Whether to send console output to log file')
+    parser.add_argument('--no-save', action='store_true',
+            help='Disable saving/copying of model.py and model.json to a unique directory for reproducibility')
+    
+    return parser.parse_args()
+
+def main(args):
+    if args.model_dest:
+        model_id = args.model_dest
+    else:
+        model_id = uuid.uuid1().hex
+
+    model_path = args.model_dir + '/' + model_id + '/'
+
+    if not args.no_save:
+        if not os.path.exists(model_path):
+            os.mkdir(model_path)
+        print("model path is " + model_path)
+
+    if args.log and not args.no_save:
+        logging.basicConfig(filename=model_path + 'model.log',
+                level=logging.DEBUG)
+        sys.stdout = LoggerWriter(logging.info)
+        sys.stderr = LoggerWriter(logging.warning)
+    else:
+        logging.basicConfig(level=logging.DEBUG)
+
+    train_file = h5py.File(args.train_file)
+    x_train = train_file['X'].value.astype(np.int32)
+    y_train = train_file[args.target_name].value.astype(np.int32)
+
+    if len(y_train) > args.n_train:
+        y_train = y_train[0:args.n_train]
+        x_train = x_train[0:args.n_train, :]
+
+    validation_file = h5py.File(args.validation_file)
+    x_validation = validation_file['X'].value.astype(np.int32)
+    y_validation = validation_file[args.target_name].value.astype(np.int32)
+
+    if len(y_validation) > args.n_validation:
+        y_validation = y_validation[0:args.n_validation]
+        x_validation = x_validation[0:args.n_validation, :]
+    
+    np.random.seed(args.seed)
+
+    n_classes = len(np.unique(y_train))
+    logging.debug("n_classes {0} min {1} max {2}".format(
+        n_classes, min(y_train), max(y_train)))
+
+    y_train_one_hot = np_utils.to_categorical(y_train, n_classes)
+    y_validation_one_hot = np_utils.to_categorical(y_validation, n_classes)
+
+    logging.debug("y_train_one_hot " + str(y_train_one_hot.shape))
+    logging.debug("x_train " + str(x_train.shape))
+
+    input_width = x_train.shape[1]
+
+    min_vocab_index = np.min(x_train)
+    max_vocab_index = np.max(x_train)
+    logging.debug("min vocab index {0} max vocab index {1}".format(
+        min_vocab_index, max_vocab_index))
+
+    json_cfg = json.load(open(args.model_dir + '/model.json'))
+    # Add a few values to the dictionary that are properties
+    # of the training data.
+    json_cfg['train_file'] = args.train_file
+    json_cfg['validation_file'] = args.validation_file
+    json_cfg['n_vocab'] = max(args.n_vocab, np.max(x_train) + 1)
+    json_cfg['input_width'] = x_train.shape[1]
+    json_cfg['n_classes'] = n_classes
+
+    logging.debug("loading model ...")
+
+    sys.path.append(args.model_dir)
+    from model import build_model
+    model_cfg = ModelConfig(**json_cfg)
+    model = build_model(model_cfg)
+
+    callbacks = []
+
+    labels = None
+    if args.labels:
+        label_dict = cPickle.load(open(args.labels))
+        labels = label_dict[args.target_name]
+
+    if not args.no_save:
+        if args.description:
+            with open(model_path + '/README.txt', 'w') as f:
+                f.write(args.description + '\n')
+
+        # Save model hyperparameters and code.
+        for model_file in ['model.py', 'model.json']:
+            shutil.copyfile(args.model_dir + '/' + model_file,
+                    model_path + '/' + model_file)
+
+        # And weights.
+        callbacks.append(ModelCheckpoint(
+            filepath=model_path + '/model.h5',
+            verbose=1,
+            save_best_only=True))
+
+    callbacks.append(EarlyStopping(
+        monitor='val_loss', patience=model_cfg.patience, verbose=1))
+
+    callback_logger = logging.info if args.log else fake_print
+
+    #def __init__(self, x, y, logger, msg='', labels=None):
+    cr = ClassificationReport(x_validation, y_validation,
+            callback_logger, msg='Validation set metrics',
+            labels=labels)
+    callbacks.append(cr)
+
+    cm = ConfusionMatrix(x_validation, y_validation,
+            callback_logger, msg='Validation set confusion matrix',
+            labels=labels)
+    callbacks.append(cm)
+
+    model.fit(x_train, y_train_one_hot,
+        shuffle=False,
+        batch_size=model_cfg.batch_size,
+        show_accuracy=True,
+        validation_data=(x_validation, y_validation_one_hot),
+        callbacks=callbacks,
+        verbose=2 if args.log else 1)
+
+if __name__ == '__main__':
+    sys.exit(main(get_parser()))
