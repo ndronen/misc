cfc6d5ab422bd741914759d33bc3c5055312aca0
Nicholas Dronen <nicholas.dronen@pearson.com>
2015-09-21 15:03:04 -0600

    Almost done with tests for SlidingWindowL2MaxPooling

--- keras/nick/nonconvnet.py
+++ keras/nick/nonconvnet.py
@@ -12,8 +12,8 @@ from keras.utils.theano_utils import sharedX
 
 class SplitOutputByFilter(Layer):
     """
-    input: (n_samples, max_sent_len, n_filters * filter_width)
-    output: (n_samples, n_filters, max_sent_len, filter_width)
+    input: (n_samples, max_seq_len, n_filters * filter_width)
+    output: (n_samples, n_filters, max_seq_len, filter_width)
     """
     def __init__(self, n_filters, filter_width):
         super(SplitOutputByFilter, self).__init__()
@@ -27,10 +27,11 @@ class SplitOutputByFilter(Layer):
         return X[:, :, start:end]
 
     def _get_output(self, X):
-        outputs, updates = theano.scan(fn=self.slice,
-            outputs_info=None,
-            sequences=[T.arange(self.n_filters)],
-            non_sequences=X)
+        outputs, updates = theano.scan(
+                fn=self.slice,
+                outputs_info=None,
+                sequences=[T.arange(self.n_filters)],
+                non_sequences=X)
         return outputs.dimshuffle(1, 0, 2, 3)
 
     def get_output(self, train):
@@ -39,32 +40,85 @@ class SplitOutputByFilter(Layer):
     def get_config(self):
         return {"name": self.__class__.__name__}
 
-class SlidingWindowL2Pooling(Layer):
-    def __init__(self, filter_width):
+class SlidingWindowL2MaxPooling(Layer):
+    '''
+    input: (n_samples, n_filters, max_seq_len, filter_width)
+    output: (n_samples, n_filters, filter_width, filter_width)
+    '''
+    def __init__(self, batch_size, n_filters, filter_width, max_seq_len):
+        self.batch_size = batch_size
+        self.n_filters = n_filters
         self.filter_width = filter_width
+        self.max_seq_len = max_seq_len
 
-    def norm_in_window(self, x, i):
-        return x[i:i+self.filter_width, :].sum()
+    def get_output(self, train):
+        return self._get_output(self.get_input(train))
 
-    def forward_cpu(self, x):
-        # The output of this layer is a 3-tensor with filters on the first
-        # dimension and the second and third each being filter width.
-        output_shape=(x[0].shape[0], self.filter_width, self.filter_width)
-        output = np.zeros(shape=output_shape, dtype=np.float32)
+    def _get_output(self, X):
+        outputs, updates = theano.scan(
+                fn=self.sample_dimension,
+                sequences=[T.arange(self.batch_size)],
+                non_sequences=X)
+        return outputs
 
-        self.indices = np.zeros(x[0].shape[0])
-        start_indices = np.arange(x[0][0, :, :].shape[0] - self.filter_width)
+    def sample_dimension(self, i, X):
+        '''
+        Takes a 4-tensor of shape `(n_samples, n_filters, max_seq_len,
+        filter_width)` and an index into its first dimension.  Returns the
+        `(n_samples, n_filters, filter_width, filter_width)` subtensor
+        with the greatest L2 norm along the third dimension.
 
-        for i in np.arange(output_shape[0]):
-            x_i = x[0][i, :, :]
-            x2 = x_i**2
-            norms = [self.norm_in_window(x2, j) for j in start_indices]
-            max_norm_index = np.argmax(norms)
-            self.indices[i] = max_norm_index
-            output[i] = x_i[max_norm_index:max_norm_index+self.filter_width, :]
+        Parameters
+        ----------
+        X : a 4-tensor
+            An `(n_samples, n_filters, max_seq_len, filter_width)` tensor.
+        i : int
+            An index into the first dimension of `X`.
 
-        logger.debug([str(val) for val in [x[0].shape, output.shape]])
-        return output,
+        Returns
+        ----------
+        A 3-tensor of shape `(n_filters, filter_width, filter_width)`
+        consisting of the subtensor of `X` with the greatest L2 norm along
+        `X`'s third dimension (where `max_seq_len` lies).
+        '''
+        outputs, updates = theano.scan(
+                fn=self.filter_dimension,
+                sequences=[T.arange(self.n_filters)],
+                non_sequences=X[i, :, :, :])
+
+        return outputs
+
+    def filter_dimension(self, i, X):
+        '''
+        Takes a 3-tensor of shape `(n_filters, max_seq_len, filter_width)`
+        and an index into its first dimension.  Returns the
+        `(filter_width, filter_width)` subtensor of `X` with the greatest
+        L2 norm along the second dimension.
+
+        Parameters
+        ----------
+        X : a 3-tensor
+            An `(n_samples, n_filters, max_seq_len, filter_width)` tensor.
+        i : int
+            An index into the first dimension of `X`.
+
+        Returns
+        ----------
+        A 2-tensor of shape `(filter_width, filter_width)` consisting
+        of the subtensor of the i-th element along the first dimension
+        of `X` with the greatest L2 norm along `X`'s second dimension
+        (where `max_seq_len` lies).
+        '''
+        norms, updates = theano.scan(
+                fn=self.norm,
+                sequences=[T.arange(self.max_seq_len)],
+                non_sequences=X[i, :, :])
+        start_window = T.argmax(norms)
+        end_window = start_window + self.filter_width
+        return X[i, start_window:end_window, :]
+
+    def norm(self, i, X):
+        return (X[i:i+self.filter_width, :] ** 2).sum()
 
 class ZeroFillDiagonals(Layer):
     def forward_cpu(self, x):
