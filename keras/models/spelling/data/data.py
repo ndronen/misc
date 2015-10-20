222186f5bf020b67e04ed5fa1eeadb1126616864
Nicholas Dronen <nicholas.dronen@pearson.com>
2015-10-20 06:58:47 -0600

    Iterate over copy of term_freq items so keys can be deleted from term_freq in loop.

--- keras/models/spelling/data/data.py
+++ keras/models/spelling/data/data.py
@@ -174,7 +174,8 @@ def build_index(token_seq, min_freq=100, max_features=1000, downsample=0):
         widgets=[' ', progressbar.Percentage(),
         ' ', progressbar.ETA()],
         maxval=len(term_freqs)).start()
-    for i, (term, freq) in enumerate(term_freqs.iteritems()):
+    term_freqs_items = term_freqs.items()
+    for i, (term, freq) in enumerate(term_freqs.items()):
         pbar.update(i+1)
 
         if freq < min_freq:
