09cf4fcfe3a1d0f76f59d692b16c340fe24dd043
Nicholas Dronen <nicholas.dronen@pearson.com>
2016-07-26 08:03:03 -0600

    Add ipython startup file.

---
+++ dotfiles/.ipython/profile_default/startup/00-first.py
@@ -0,0 +1,24 @@
+try:
+    import numpy as np
+except ImportError:
+    pass
+
+try:
+    import scipy as sp
+except ImportError:
+    pass
+
+try:
+    import sklearn as skl
+except ImportError:
+    pass
+
+try:
+    import pandas as pd
+except ImportError:
+    pass
+
+try:
+    import pickle
+except ImportError:
+    pass
